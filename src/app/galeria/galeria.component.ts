e4ad4004f820353398a2c04088b338734742c58b
Welsey  Neri Bernadoque <admtelemetria@AUTOSENDER.AUTOSENDER.COM.BR>
2020-10-08 13:15:09 -0300

    Projeto finalizado e refatorado

---
+++ src/app/galeria/galeria.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css'],
+})
+export class GaleriaComponent {
+  @Input() foto: string[];
+  @Input() titulo: string;
+  next() {
+    this.fotoAtual++;
+  }
+
+  previous() {
+    this.fotoAtual--;
+  }
+  fotoAtual: number = 0;
+}
